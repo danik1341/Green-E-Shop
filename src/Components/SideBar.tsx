aee40def157305395436dfb53e67803386d92cdc
Daniel <Yevlanov@icloud.com>
2024-04-04 00:03:14 +0300

    fixed sidebar

--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -2,6 +2,7 @@ import { CiDollar } from "react-icons/ci";
 import { HiOutlineMapPin } from "react-icons/hi2";
 import Logo from "../assets/Logo.svg";
 import { AboveNavBarMobile } from "./AboveNavBar";
+
 function SideBarMobile() {
   // {Sidebar for devices smaller that XL screen}
   return (
@@ -15,32 +16,30 @@ function SideBarMobile() {
         <div className="mt-4 me-3">
           <AboveNavBarMobile />
         </div>
-        <div>
-          <button
-            data-drawer-target="logo-sidebar"
-            data-drawer-toggle="logo-sidebar"
-            aria-controls="logo-sidebar"
-            type="button"
-            className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          >
-            <span className="sr-only">Open sidebar</span>
-            <svg
-              className="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                clip-rule="evenodd"
-                fill-rule="evenodd"
-                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-              ></path>
-            </svg>
-          </button>
-        </div>
-      </div>
 
+        <button
+          data-drawer-target="logo-sidebar"
+          data-drawer-toggle="logo-sidebar"
+          aria-controls="logo-sidebar"
+          type="button"
+          className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        >
+          <span className="sr-only">Open sidebar</span>
+          <svg
+            className="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clip-rule="evenodd"
+              fill-rule="evenodd"
+              d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+            ></path>
+          </svg>
+        </button>
+      </div>
       <aside
         id="logo-sidebar"
         className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
@@ -48,7 +47,7 @@ function SideBarMobile() {
       >
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
           <a href="#" className="flex items-center ps-2.5 mb-5">
-            <img src={Logo} className="h-6 me-3 sm:h-7" alt="Flowbite Logo" />
+            <img src={Logo} className="h-6 me-3 sm:h-7" alt="EcoBazar Logo" />
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
               EcoBazar
             </span>

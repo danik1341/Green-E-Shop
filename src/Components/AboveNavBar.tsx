31b209252e90cd870e3f147b14cf3b2488a606e9
Daniel <Yevlanov@icloud.com>
2024-04-09 19:00:22 +0300

    rework

--- src/Components/AboveNavBar.tsx
+++ src/Components/AboveNavBar.tsx
@@ -1,30 +1,33 @@
 import { HiOutlineMapPin } from "react-icons/hi2";
-import { IoIosHeartEmpty } from "react-icons/io";
-import { SlHandbag } from "react-icons/sl";
 import ReactLoginModel from "./Modals";
 
 function AboveNavBarLargeScreen() {
   return (
     <div
       style={{
-        backgroundColor: `#333333`,
+        backgroundColor: `#EDF2EE`,
       }}
       className="max-w-screen-xxl h-11 flex-wrap align-baseline justify-between mx-auto lg:mx-0 hidden lg:flex"
     >
       {/* {Country selector} */}
       <div id="location-selector" className="ms-14 flex">
-        <div>
-          <HiOutlineMapPin className="text-slate-400 mt-3.5 me-1" />
+        <div
+          style={{
+            color: `#7A997C`,
+          }}
+        >
+          <HiOutlineMapPin className=" mt-3.5 me-1" />
         </div>
         <form className="max-w-sm mx-auto">
           {/* {Country selector} */}
           <label htmlFor="underline_select" className="sr-only"></label>
           <select
-            id="underline_select"
             style={{
               background: `none`,
+              color: `#7A997C`,
             }}
-            className="block py-2.5 px-0 mt-0.5 w-1/1 text-sm text-gray-500 bg-transparent  border-0 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+            className="block py-2.5 px-0 mt-0.5 w-1/1 text-sm 
+             bg-transparent  border-0 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
           >
             <option selected>Store Location</option>
             <option value="US">United States</option>
@@ -44,6 +47,7 @@ function AboveNavBarLargeScreen() {
               id="underline_select"
               style={{
                 background: `none`,
+                color: `#7A997C`,
               }}
               className="block px-12 py-2.5 mt-0.5  w-1/1 text-sm text-gray-500 bg-transparent border-0 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
             >
@@ -59,6 +63,7 @@ function AboveNavBarLargeScreen() {
         <div
           style={{
             backgroundColor: `gray`,
+            color: `#7A997C`,
           }}
           className="border-r h-5 mt-3 border-gray-500"
         ></div>
@@ -70,6 +75,7 @@ function AboveNavBarLargeScreen() {
               id="underline_select"
               style={{
                 background: `none`,
+                color: `#7A997C`,
               }}
               className="block px-12 mt-0.5 py-2.5 w-1/1 text-sm text-gray-500 bg-transparent border-0   appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
             >
@@ -80,18 +86,17 @@ function AboveNavBarLargeScreen() {
             </select>
           </form>
         </div>
-        <div
-          style={{
-            backgroundColor: `gray`,
-          }}
-          className="border-r h-5 mt-3 border-gray-500"
-        ></div>
+        <div className="border-r h-5 mt-3 border-gray-500"></div>
         {/* {Currency Selector} */}
         <div
           id="login-logout-container"
           className="flex mt-0.5 ms-10 text-sm text-gray-500"
         >
-          <div>
+          <div
+            style={{
+              color: `#7A997C`,
+            }}
+          >
             <ReactLoginModel />
           </div>
         </div>
@@ -100,36 +105,4 @@ function AboveNavBarLargeScreen() {
   );
 }
 
-function AboveNavBarMobile() {
-  return (
-    <div className="">
-      {/* {card icon + like icon} */}
-      <div className="flex gap-2">
-        <div>
-          <a href="/Liked">
-            <button>
-              <IoIosHeartEmpty className="  text-gray-500 w-5 h-5" />
-            </button>
-          </a>
-        </div>
-        <div
-          style={{
-            backgroundColor: `gray`,
-          }}
-          className="border-r border-gray-500"
-        ></div>
-        <div>
-          <a href="/Cart">
-            <button>
-              <SlHandbag className="  text-gray-500 w-4 h-5" />
-              <span className="absolute top-7 right-2 bg-red-500 text-white px-1 text-2xs rounded-full text-xs">
-                1
-              </span>
-            </button>
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-}
-export { AboveNavBarLargeScreen, AboveNavBarMobile };
+export { AboveNavBarLargeScreen };
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,15 +1,9 @@
 import { AboveNavBarLargeScreen } from "../Components/AboveNavBar";
-import { BelowNavBarLargeScreens } from "../Components/BelowNavBar";
-import { NavBar } from "../Components/NavBar";
-import { SideBarMobile } from "../Components/SideBar";
 
 function HomePage() {
   return (
     <div>
       <AboveNavBarLargeScreen />
-      <SideBarMobile />
-      <NavBar />
-      <BelowNavBarLargeScreens />
     </div>
   );
 }
